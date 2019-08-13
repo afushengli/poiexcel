245eb2f5fe6097466d13f07ec0082e9963747ecf
chenrujun <chenrj@360bid.cn>
2019-08-13 11:01:39 +0800

    提交代码

--- src/main/java/com/fsl/poiexcel/PoiexcelApplication.java
+++ src/main/java/com/fsl/poiexcel/PoiexcelApplication.java
@@ -13,6 +13,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -67,6 +68,16 @@ public class PoiexcelApplication extends WebMvcConfigurerAdapter {
         return new AccessLimitInterceptor();
     }
 
+
+    //允许跨域
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
 
 
 }
