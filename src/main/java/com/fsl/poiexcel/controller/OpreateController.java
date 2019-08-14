b6955f31149b2ef83072af97aca29894e14a9d53
chenrujun <chenrj@360bid.cn>
2019-08-14 15:54:37 +0800

    改为post请求

--- src/main/java/com/fsl/poiexcel/controller/OpreateController.java
+++ src/main/java/com/fsl/poiexcel/controller/OpreateController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -37,7 +38,7 @@ public class OpreateController {
 
 
 
-    @RequestMapping(value = "/downFileFromBack")
+    @PostMapping(value = "/downFileFromBack")
     @ResponseBody
     public void downFileFromBack(String fileName,
                                  HttpServletResponse response) throws IOException {
